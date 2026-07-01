8693c4b47e663114acc414afa4bec575b901159d
raphaelamaffouo41 <raphaelamaffouo41@gmail.com>
2026-07-01 18:13:01 +0100

    creation_page_login

--- PhotoEyes/PhotoEyes_frontend/src/app/app.routes.ts
+++ PhotoEyes/PhotoEyes_frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from "./authentification/login/login.component";
+import { RegisterComponent } from "./authentification/register/register.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+
+  {path: '', redirectTo: 'login', pathMatch: 'full'}
+];
