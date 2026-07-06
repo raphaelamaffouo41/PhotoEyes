f1e3c0d4c5ebc9ff504e1bf994a6879b6506df4f
raphaelamaffouo41 <raphaelamaffouo41@gmail.com>
2026-07-06 14:29:52 +0100

    page_inscription

--- PhotoEyes/PhotoEyes_frontend/src/app/authentification/pages/register/register.component.ts
+++ PhotoEyes/PhotoEyes_frontend/src/app/authentification/pages/register/register.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common"
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  isOpen = true;
 }
